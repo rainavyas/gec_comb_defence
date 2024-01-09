865eb1989cf24d149e12fc5c5071aad7c03082c2
Vyas Raina <sayvaniar@gmail.com>
2024-01-09 11:44:56 +0000

    debug

--- src/combination/mbr.py
+++ src/combination/mbr.py
@@ -11,7 +11,7 @@ class MBRcombiner:
     def _make_all_changes(cls, source_sentences, pred_texts):
         selected_samples = []
         # for n, samples in tqdm(enumerate(zip(*pred_texts)), total=len(source_sentences)):
-        for n, samples in enumerate(zip(*pred_texts)), total=len(source_sentences):
+        for n, samples in enumerate(zip(*pred_texts)):
             edits = [cls.return_edits(source_sentences[n], s) for s in samples]
             best = [None, -1] # [model index, score] 
             for i in range(len(edits)):
